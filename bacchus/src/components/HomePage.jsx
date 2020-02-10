d7ae62419489fbb07b4581f39db372b2b4703122
Celine Desnoe <celine.desnoe@weloop.io>
2020-02-10 12:14:08 +0100

    [ADD] few thing on homepage

--- bacchus/src/components/HomePage.jsx
+++ bacchus/src/components/HomePage.jsx
@@ -4,14 +4,21 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: "", load: false
+      content: "", isLoaded: false
     };
   }
+
+componentDidMount () {
+setTimeout(() => {this.setState({isLoaded : true})}, 1000)
+}
+
   render() {
+    let {isLoaded} = this.state;
     return (
-      <div className="bg-primary background-home text-white">
-          <div>HOMEPAGE</div>
-          <div className="loader"></div>
+      <div className="bg-primary background-home text-white d-flex align-items-center justify-content-center flex-column">
+          {isLoaded && <div className="mb-4"><div className="title-bacchus d-flex align-items-center justify-content-center">Bacchus</div><div className="subtitle">Know your wine</div></div>}
+          <div className={`loader ${isLoaded ? "loader-homepage-done" :"loader-homepage"} `}></div>
+          {isLoaded && <div className="log-in-box"><div>Log in to your cellar</div></div>}
       </div>
     );
   }
