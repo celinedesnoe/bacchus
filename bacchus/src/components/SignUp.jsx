7335eae8837f64aecec9c556f934126dd6d14d6b
Celine <celine.desnoe@weloop.io>
2020-02-28 15:18:40 +0100

    [ADD] onChange for Sign up

--- bacchus/src/components/SignUp.jsx
+++ bacchus/src/components/SignUp.jsx
@@ -3,6 +3,12 @@ import Input from "./Input.jsx";
 
 const SignUp = props => {
   const [step, setStep] = useState(1);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cellar, setCellar] = useState("");
+  const [capacity, setCapacity] = useState(0);
+
   return (
     <>
       <div className="position-absolute card log-in-box d-flex align-items-center flex-column mt-3">
@@ -12,13 +18,21 @@ const SignUp = props => {
               <h4 className="my-4 text-dark">Create your account</h4>
               <form onSubmit={console.log("SUBMIT")} className="w-100">
                 <div className="w-100">
-                  <Input placeholder="John" className="mb-3" title="Name" />
+                  <Input
+                    placeholder="John"
+                    className="mb-3"
+                    title="Name"
+                    onChange={e => setName(e.target.value)}
+                    value={name}
+                  />
                 </div>
                 <div className="w-100">
                   <Input
                     placeholder="john.doe@gmail.com"
                     className="mb-3"
                     title="E-mail"
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
                   />
                 </div>
                 <div className="w-100">
@@ -27,6 +41,8 @@ const SignUp = props => {
                     className="mb-3"
                     title="Password"
                     type="password"
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
                   />
                 </div>
                 <div className="w-100 mt-5">
@@ -47,9 +63,11 @@ const SignUp = props => {
               <form onSubmit={console.log("SUBMIT")} className="w-100">
                 <div className="w-100">
                   <Input
-                    placeholder="John"
+                    placeholder="Home"
                     className="mb-3"
                     title="Name of your cellar"
+                    onChange={e => setCellar(e.target.value)}
+                    value={cellar}
                   />
                 </div>
                 <div className="w-100">
@@ -58,6 +76,8 @@ const SignUp = props => {
                     className="mb-3"
                     title="Capacity"
                     type="number"
+                    onChange={e => setCapacity(e.target.value)}
+                    value={capacity}
                   />
                 </div>
 
@@ -65,7 +85,7 @@ const SignUp = props => {
                   <div
                     className="button"
                     onClick={e => {
-                      setStep(2);
+                      console.log(name, email, password, cellar, capacity);
                     }}
                   >
                     Ready to start?{" "}
