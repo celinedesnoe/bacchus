c001e7d6ae48d121bcee8dbd3fc48e8bc0384a28
Celine <celine.desnoe@weloop.io>
2020-03-30 16:22:29 +0200

    fix styles for empty dashboard

--- bacchus/src/components/EmptyDashboard.jsx
+++ bacchus/src/components/EmptyDashboard.jsx
@@ -5,15 +5,15 @@ import Button from "./Button";
 const EmptyDashboard = props => {
   return (
     <div className="card empty-dashboard-box d-flex align-items-center flex-column mt-3">
-      <div>No bottles yet?</div>
-      <div>
+      <div className="my-3 extrabold">No bottles yet?</div>
+      <div className="mb-3">
         <img
           src="/two-bottles-yellow.svg"
           className="two-bottles"
           al="two-bottles"
         />
       </div>
-      <Button text="Add a bottle" />
+      <Button text="Add a bottle" className="my-2" />
     </div>
   );
 };
