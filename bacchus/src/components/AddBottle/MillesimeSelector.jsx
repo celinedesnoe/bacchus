266214401715a0617e686d1aaaac815bdcfb02e4
Celine <celine.desnoe@weloop.io>
2020-04-06 17:12:58 +0200

    add cursor-pointer

--- bacchus/src/components/AddBottle/MillesimeSelector.jsx
+++ bacchus/src/components/AddBottle/MillesimeSelector.jsx
@@ -14,20 +14,20 @@ const MillesimeSelector = ({ addMillesime, bottleMillesime }) => {
       <div className="label-input mb-2">Millésime</div>
       <div className="d-flex align-items-center">
         <div
-          className="text-primary-light"
+          className="text-primary-light cursor-pointer"
           onClick={() => setMillesime(millesime - 1)}
         >
           <FontAwesomeIcon icon={["fa", "chevron-left"]} />
         </div>
         <div className="millesime-card py-2 px-3 mx-3">{millesime}</div>
         <div
-          className="text-primary-light"
+          className="text-primary-light cursor-pointer"
           onClick={() => setMillesime(millesime + 1)}
         >
           <FontAwesomeIcon icon={["fa", "chevron-right"]} />
         </div>
         <div
-          className="millesime-card py-2 px-3 mx-3"
+          className="millesime-card py-2 px-3 mx-3 cursor-pointer"
           onClick={() => setMillesime(null)}
         >
           N/A
--- bacchus/src/components/AddButton.jsx
+++ bacchus/src/components/AddButton.jsx
@@ -5,7 +5,7 @@ import { history } from "../_helpers/history";
 const AddButton = () => {
   return (
     <div
-      className="add-button d-flex align-items-center justify-content-center"
+      className="add-button d-flex align-items-center justify-content-center cursor-pointer"
       onClick={() => history.push("/new-bottle")}
     >
       <FontAwesomeIcon icon={["fa", "plus"]} />
