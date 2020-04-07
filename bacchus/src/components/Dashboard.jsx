5c74b1c73706388d38cbbccf19481f74dfa63d55
Celine <celine.desnoe@weloop.io>
2020-04-07 10:39:26 +0200

    component for header dashboard

--- bacchus/src/components/Dashboard.jsx
+++ bacchus/src/components/Dashboard.jsx
@@ -19,41 +19,8 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="">
-        <div className="header-dashboard px-3 py-2">
-          <div className="d-flex justify-content-between mt-3">
-            <div className="">
-              <h3 className="">Welcome Céline,</h3>
-              <h5>
-                You have
-                <span className="text-secondary nb-bottles">
-                  {` ${this.props.bottles.length} `}
-                </span>
-                bottles in stock
-              </h5>
-            </div>
-            <div
-              className={`profile-image mr-3`}
-              style={{
-                backgroundImage: `url(https://randomuser.me/api/portraits/women/90.jpg)`
-              }}
-            />
-          </div>
-          <Search placeholder="Name, Region, Appellation..." className="" />
-          <div className="d-flex my-3">
-            <Button
-              text="All"
-              className="mr-2"
-              selected={this.state.filterSelected === "all"}
-              filter={true}
-            />
-            <Button
-              text="In stock"
-              className=""
-              selected={this.state.filterSelected === "instock"}
-              filter={true}
-            />
-          </div>
-        </div>
+        <HeaderDashboard filterSelected={this.state.filterSelected} />
+
         <div className="d-flex justify-content-center">
           {this.props.bottles.length === 0 ? (
             <EmptyDashboard />
---
+++ bacchus/src/components/HeaderDashboard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { history } from "../_helpers/history";
+
+const HeaderDashboard = () => {
+  return (
+    <div className="header-dashboard px-3 py-2">
+      <div className="d-flex justify-content-between mt-3">
+        <div className="">
+          <h3 className="">Welcome {props.user.firstName},</h3>
+          <h5>
+            You have
+            <span className="text-secondary nb-bottles">
+              {` ${props.bottles.length} `}
+            </span>
+            bottles in stock
+          </h5>
+        </div>
+        <div
+          className={`profile-image mr-3`}
+          style={{
+            backgroundImage: `url(https://randomuser.me/api/portraits/women/90.jpg)`
+          }}
+        />
+      </div>
+      <Search placeholder="Name, Region, Appellation..." className="" />
+      <div className="d-flex my-3">
+        <Button
+          text="All"
+          className="mr-2"
+          selected={props.filterSelected === "all"}
+          filter={true}
+        />
+        <Button
+          text="In stock"
+          className=""
+          selected={props.filterSelected === "instock"}
+          filter={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  const { authReducer, bottlesReducer } = state;
+  return { user: authReducer, bottles: bottlesReducer };
+};
+
+export default connect(mapStateToProps)(HeaderDashboard);
