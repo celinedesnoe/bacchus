e942eca3195abe85d7db8033691377768593d296
Celine <celine.desnoe@weloop.io>
2020-04-08 15:54:00 +0200

    fix counter for millesim with an input

--- bacchus/src/components/AddBottle/MillesimeSelector.jsx
+++ bacchus/src/components/AddBottle/MillesimeSelector.jsx
@@ -3,12 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MillesimeSelector = ({ addMillesime, bottleMillesime }) => {
   const [millesime, setMillesime] = useState(
-    bottleMillesime ? bottleMillesime : 2015
+    bottleMillesime ? bottleMillesime : null
   );
   useEffect(() => {
     addMillesime(millesime);
   }, [millesime]);
 
+  console.log(typeof millesime);
   return (
     <div className="mb-3">
       <div className="label-input mb-2">Millésime</div>
@@ -16,16 +17,25 @@ const MillesimeSelector = ({ addMillesime, bottleMillesime }) => {
         <div className="d-flex align-items-center">
           <div
             className="counter-icon text-primary-light cursor-pointer"
-            onClick={() => setMillesime(millesime - 1)}
+            onClick={() => setMillesime(!millesime ? 2015 : millesime - 1)}
           >
             <FontAwesomeIcon icon={["fa", "chevron-left"]} />
           </div>
-          <div className="millesime-card py-2 px-3 mx-2">
-            {millesime ? millesime : "?"}
+          <div className="millesime-card px-3 mx-2">
+            <input
+              value={!millesime ? "?" : millesime}
+              onChange={e => {
+                let value = parseInt(e.target.value);
+                setMillesime(value);
+              }}
+              type="number"
+              placeholder="2015"
+              className="input-inline form-control"
+            />
           </div>
           <div
             className="counter-icon text-primary-light cursor-pointer"
-            onClick={() => setMillesime(millesime + 1)}
+            onClick={() => setMillesime(!millesime ? 2015 : millesime + 1)}
           >
             <FontAwesomeIcon icon={["fa", "chevron-right"]} />
           </div>
