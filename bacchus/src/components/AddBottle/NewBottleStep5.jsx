7acd1ee93d1826c34e65994e0723026b2a006b1c
Celine <celine.desnoe@weloop.io>
2020-04-08 15:34:41 +0200

    unknow data for bottle except name

--- bacchus/src/components/AddBottle/NewBottleStep5.jsx
+++ bacchus/src/components/AddBottle/NewBottleStep5.jsx
@@ -31,40 +31,52 @@ const NewBottleStep5 = ({ bottle, setDetails }) => {
         <div className="pl-5 mt-3">
           <div>{bottle.name}</div>
           <div className="d-flex row mt-4">
-            <div className="col-6">
-              <div className="label-input pb-1 mr-4">Millésime</div>
-              <div className="">
-                {bottle.millesime ? bottle.millesime : "?"}
+            {bottle.millesime && (
+              <div className="col-6">
+                <div className="label-input pb-1 mr-4">Millésime</div>
+                <div className="">{bottle.millesime}</div>
               </div>
-            </div>
-            <div className="col-6">
-              <div className="label-input pb-1">Origin</div>
-              <div className="">
-                {bottle.region ? bottle.region : "?"},{" "}
-                {bottle.country ? bottle.country : "?"}
+            )}
+            {(bottle.region || bottle.country) && (
+              <div className="col-6">
+                <div className="label-input pb-1">Origin</div>
+                <div className="">
+                  {bottle.region ? bottle.region : ""}
+                  {bottle.country
+                    ? bottle.region
+                      ? ", " + bottle.country
+                      : bottle.country
+                    : ""}
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="d-flex row mt-3">
-            <div className="col-6">
-              <div className="label-input pb-1 mr-4">Appellation</div>
-              <div className="">
-                {bottle.appellation ? bottle.appellation : "N/A"}
+            {bottle.appellation && (
+              <div className="col-6">
+                <div className="label-input pb-1 mr-4">Appellation</div>
+                <div className="">{bottle.appellation}</div>
               </div>
-            </div>
-            <div className="col-6">
-              <div className="label-input pb-1">Cépage</div>
-              <div className="">{bottle.cepage ? bottle.cepage : "N/A"}</div>
-            </div>
+            )}
+            {bottle.cepage && (
+              <div className="col-6">
+                <div className="label-input pb-1">Cépage</div>
+                <div className="">{bottle.cepage ? bottle.cepage : "N/A"}</div>
+              </div>
+            )}
           </div>
         </div>
         <div className="d-flex justify-content-between mt-4">
-          <div className="badge badge-primary">
-            {bottle.nb ? bottle.nb : "?"} bottles
-          </div>
-          <div className="badge badge-tertiary">
-            {bottle.price ? bottle.price : "?"} $
-          </div>
+          {bottle.nb && (
+            <div className="badge badge-primary">
+              {bottle.nb} bottle{bottle.nb > 1 && "s"}
+            </div>
+          )}
+          {bottle.price && (
+            <div className="badge badge-tertiary">
+              {bottle.price ? bottle.price : "?"} $
+            </div>
+          )}
         </div>
       </div>
     </div>
