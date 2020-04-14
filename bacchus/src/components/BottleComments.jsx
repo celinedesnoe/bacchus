46cae30368348ce30aa63db23bd396272e656829
Celine <celine.desnoe@weloop.io>
2020-04-14 16:09:48 +0200

    add condition for button add ocment with shadow

--- bacchus/src/components/BottleComments.jsx
+++ bacchus/src/components/BottleComments.jsx
@@ -1,34 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CommentCard from "./CommentCard.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CommentsEmptyState from "./CommentsEmptyState.jsx";
 
 const comments = [
-  { comment: "Great wine", date: "23/04/2020", note: "90/100" },
+  { comment: "Great wine", date: "23/04/2020", note: "90/100", id: 23 },
   {
     comment: "Need to open it 2 hours before",
     date: "02/02/2020",
-    note: "65/100"
+    note: "65/100",
+    id: 24
   },
-  { comment: "Great wine", date: "23/04/2020", note: "90/100" },
+  { comment: "Great wine", date: "23/04/2020", note: "90/100", id: 25 },
   {
     comment: "Need to open it 2 hours before",
     date: "02/02/2020",
-    note: "65/100"
+    note: "65/100",
+    id: 36
   }
 ];
 
+const emptyStateComments = comments.length === 0;
+
 const BottleComments = () => {
+  const [buttonClass, setButtonClass] = useState("");
+  const refList = useRef();
+  const refAllComments = useRef();
+
+  const addShadowButton = () => {
+    const allCommentsWidth = refAllComments.current.offsetWidth;
+    const listWidth = refList.current.offsetWidth;
+    if (allCommentsWidth > listWidth) {
+      setButtonClass("shadow");
+    } else {
+      setButtonClass("");
+    }
+  };
+
+  useEffect(() => {
+    addShadowButton();
+
+    if (refList.current) {
+      window.addEventListener("resize", addShadowButton);
+    }
+
+    return () => {
+      window.removeEventListener("resize", addShadowButton);
+    };
+  }, [refAllComments]);
+
   return (
-    <div>
+    <div className="mt-4">
       <div class="label-input pb-1 mr-4 mb-2">Comments</div>
 
-      <div className="d-flex">
-        <div className="list-comments d-flex">
-          {comments.map(comment => (
-            <CommentCard comment={comment} key={comment.comment} />
-          ))}
+      <div className="d-flex align-items-center">
+        <div className="list-comments d-flex" ref={refList}>
+          {emptyStateComments ? (
+            <CommentsEmptyState />
+          ) : (
+            <div className="d-flex" ref={refAllComments}>
+              {comments.map(comment => (
+                <CommentCard comment={comment} key={comment.id} />
+              ))}
+            </div>
+          )}
         </div>
-        <div className="button-add-comment d-flex justify-content-center align-items-center position-relative ml-3 mb-3">
+        <div
+          className={`${buttonClass} button-add-comment d-flex justify-content-center align-items-center position-relative ml-3 py-2`}
+        >
           <FontAwesomeIcon icon={["fa", "plus"]} />
         </div>
       </div>
---
+++ bacchus/src/components/CommentsEmptyState.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CommentsEmptyState = () => {
+  return (
+    <div className="">
+      <img src="/comment-empty-state.svg" alt="comment-empty-state" />
+      <div>No comment? Add one!</div>
+    </div>
+  );
+};
+
+export default CommentsEmptyState;
